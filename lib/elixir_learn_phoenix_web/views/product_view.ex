6901111a7ecbe77aacdbe3647b3b436d84d7c875
tho.phan <tho.phan@onpoint.vn>
2023-09-18 15:31:57 +0700

    Add process paginate

--- lib/elixir_learn_phoenix_web/views/product_view.ex
+++ lib/elixir_learn_phoenix_web/views/product_view.ex
@@ -1,3 +1,26 @@
 defmodule ElixirLearnPhoenixWeb.ProductView do
   use ElixirLearnPhoenixWeb, :view
+
+  def calculate_pagination(%{
+        total: total,
+        page: page,
+        limit: limit
+      }) do
+    total_page = round(Float.ceil(total / limit))
+
+   case total_page <= 0 do
+        true ->
+          []
+
+        false ->
+          Enum.reduce(1..total_page, [], fn p, acc ->
+            if abs(p - page) <= 2 or p in [1, total_page] do
+              acc ++ [p]
+            else
+              acc ++ ["..."]
+            end
+          end)
+          |> Enum.dedup()
+      end
+  end
 end
